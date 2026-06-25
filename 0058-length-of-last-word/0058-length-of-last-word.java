2388f4ad9fecc8a014244da5a8ba0373f02d3640
Deepappriya <deepappriyarenganathan22@gmail.com>
2026-06-25 20:05:02 +0530

    Time: 0 ms (100%), Space: 42.9 MB (83.35%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,6 +1,17 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] str=s.split(" +");
-        return str[str.length-1].length();
+        // String[] str=s.split(" +");
+        // return str[str.length-1].length();
+        s=s.trim();
+        int c=0;
+        for(int i=s.length()-1;i>=0;i--){
+            if(s.charAt(i)!=' '){
+                c++;
+            }
+            else if(c>0){
+                break;
+            }
+        }
+        return c;
     }
 }

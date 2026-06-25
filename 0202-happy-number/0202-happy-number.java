fd331669c746444b42751842e66e535a31cbf80a
Deepappriya <deepappriyarenganathan22@gmail.com>
2026-06-25 21:14:09 +0530

    Time: 0 ms (100%), Space: 42.1 MB (94.42%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isHappy(int n) {
+        while(n>9)
+        {   int sum=0;
+            while(n!=0){ 
+                sum+=Math.pow(n%10,2);
+                n/=10;
+            }
+            n=sum;
+        }
+        return n==1 || n==7;
+    }
+}

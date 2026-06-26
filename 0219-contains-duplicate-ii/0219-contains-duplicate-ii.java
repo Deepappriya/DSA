01fe1d7ad95b6e4bece0a4f44049209e2aef1ec8
Deepappriya <deepappriyarenganathan22@gmail.com>
2026-06-26 14:37:49 +0530

    Time: 23 ms (79.38%), Space: 90.7 MB (27.4%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,25 +1,25 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        // HashMap<Integer,Integer> h=new HashMap<>();
-        // for(int i=0;i<nums.length;i++){
-        //     if(h.containsKey(nums[i])){
-        //         int pre=h.get(nums[i]);
-        //         if(i-pre <=k)
-        //             return true;
-        //     }
-        //     h.put(nums[i],i);
-        // }
-        // return false;
-
-        Set<Integer> s=new HashSet<>();
+        HashMap<Integer,Integer> h=new HashMap<>();
         for(int i=0;i<nums.length;i++){
-            if(i>k){
-                s.remove(nums[i-k-1]);
+            if(h.containsKey(nums[i])){
+                int pre=h.get(nums[i]);
+                if(i-pre <=k)
+                    return true;
             }
-            if(s.contains(nums[i]))
-                return true;
-            s.add(nums[i]);
+            h.put(nums[i],i);
         }
         return false;
+
+        // Set<Integer> s=new HashSet<>();
+        // for(int i=0;i<nums.length;i++){
+        //     if(i>k){
+        //         s.remove(nums[i-k-1]);
+        //     }
+        //     if(s.contains(nums[i]))
+        //         return true;
+        //     s.add(nums[i]);
+        // }
+        // return false;
     }
 }

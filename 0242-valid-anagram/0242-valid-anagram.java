69c9f674d506d35d7f699e08b41d2db3d596f1df
Deepappriya <deepappriyarenganathan22@gmail.com>
2026-06-26 16:09:58 +0530

    Time: 5 ms (61.9%), Space: 44.1 MB (89.32%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -6,27 +6,27 @@ class Solution {
         // Arrays.sort(b);
         // return Arrays.equals(a,b);
 
-        // if(s.length()!=t.length()) return false;
-        // int[] a=new int[26];
-        // for(int i=0;i<s.length();i++){
-        //     a[s.charAt(i)-'a']++;
-        //     a[t.charAt(i)-'a']--;
-        // }
-        // for(int i:a){
-        //     if(i!=0)
-        //     return false;
-        // }
-        // return true;
-
-        if(s.length()!=t.length()) 
+        if(s.length()!=t.length()) return false;
+        int[] a=new int[26];
+        for(int i=0;i<s.length();i++){
+            a[s.charAt(i)-'a']++;
+            a[t.charAt(i)-'a']--;
+        }
+        for(int i:a){
+            if(i!=0)
             return false;
+        }
+        return true;
 
-        Map<Character, Integer> hm1 = new HashMap<>();
-        Map<Character, Integer> hm2 = new HashMap<>();
-        for(char c:s.toCharArray())
-            hm1.put(c,hm1.getOrDefault(c,0)+1);
-        for(char c:t.toCharArray())
-            hm2.put(c,hm2.getOrDefault(c,0)+1);
-        return hm1.equals(hm2);
+        // if(s.length()!=t.length()) 
+        //     return false;
+
+        // Map<Character, Integer> hm1 = new HashMap<>();
+        // Map<Character, Integer> hm2 = new HashMap<>();
+        // for(char c:s.toCharArray())
+        //     hm1.put(c,hm1.getOrDefault(c,0)+1);
+        // for(char c:t.toCharArray())
+        //     hm2.put(c,hm2.getOrDefault(c,0)+1);
+        // return hm1.equals(hm2);
     }
 }
